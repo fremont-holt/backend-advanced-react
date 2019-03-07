b5462b0f58e5020a8ae9feaee76e7cd924cc927a
Tim Holt <fremont.holt@gmail.com>
2019-03-06 20:24:30 -0500

    Adding graphql and prisma servers. Adding Item type mutator and query.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,21 @@
-const mutations = {};
+const Mutations = {
+  // async await to make sure that item is created in db before returning
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   console.log(args);
+  // }
+};
+
+module.exports = Mutations;
