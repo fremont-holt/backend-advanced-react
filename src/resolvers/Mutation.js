35dbf04d3591928f2a3f78fdd96177ef1f13a64e
Tim Holt <fremont.holt@gmail.com>
2019-03-27 21:56:05 -0400

    Adding new queries and models for users/items

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   // async await to make sure that item is created in db before returning
   async createItem(parent, args, ctx, info) {
@@ -12,10 +15,62 @@ const Mutations = {
     );
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the id from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item(
+      { where },
+      `
+    {
+      id
+      title
+    }`
+    );
+    // check if they own that item or have the permissions
+    // TODO
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    // create the JWT for them
+    const token = jwt.sign({ userID: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 100 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return the user to the browser
+    return user;
   }
-  // createDog(parent, args, ctx, info) {
-  //   console.log(args);
-  // }
 };
 
 module.exports = Mutations;
